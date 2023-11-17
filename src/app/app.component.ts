e9d49e7a7a3c50a376f1bcf5dce3e353f8ca7397
Arjun <arjunp1601@gmail.com>
2023-11-17 14:19:46 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+// this is root component whose html content is pased in index.html file
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template: '<h1>Hello World</h1>',         // either give templateUrl or directly provide html
+  styleUrls: ['./app.component.css']       // local css file
 })
-export class AppComponent {
+export class AppComponent { 
   title = 'myapp';
 }
